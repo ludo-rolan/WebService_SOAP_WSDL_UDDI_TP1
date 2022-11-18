16cc75ff11a85066b48fee262651e4006d5f60ea
ludo-rolan <ekallejrl@gmail.com>
2022-11-18 17:18:01 +0100

    New Annotations added to Compte class

--- src/main/java/ws/Compte.java
+++ src/main/java/ws/Compte.java
@@ -1,11 +1,19 @@
 package ws;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlTransient;
+
 import java.util.Date;
 
+@XmlRootElement(name = "compte")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Compte {
 
     private int code;
     private double solde;
+    @XmlTransient
     private Date dateCreation;
 
     public Compte() {
